8832d922ef6496ddb2e250ae43967d7a09c78fdf
sai krishna <saikrishnamamidi2003@gmail.com>
2026-03-06 17:59:33 +0530

    fImplement Create Account page UI to match design

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -5,31 +5,56 @@ function CreateAccount(){
   const navigate = useNavigate();
 
   return(
-    <div className="create-container">
+    <div className="create-page">
 
-      <h2>Create your PopX account</h2>
+      <h1 className="create-title">
+        Create your <br /> PopX account
+      </h1>
 
-      <input type="text" placeholder="Full Name*" />
-      <input type="number" placeholder="Phone number*" />
-      <input type="email" placeholder="Email address*" />
-      <input type="password" placeholder="Password*" />
-      <input type="text" placeholder="Company name" />
-
-      <div className="agency">
-        <p>Are you an Agency?*</p>
-
-        <label>
-          <input type="radio" name="agency"/> Yes
-        </label>
-
-        <label>
-          <input type="radio" name="agency"/> No
-        </label>
+      <div className="form-group">
+        <label>Full Name<span>*</span></label>
+        <input type="text" placeholder="Marry Doe" />
       </div>
 
-      <button 
-        className="create-btn"
-        onClick={()=> navigate("/accountSettings")}
+      <div className="form-group">
+        <label>Phone number<span>*</span></label>
+        <input type="text" placeholder="Marry Doe" />
+      </div>
+
+      <div className="form-group">
+        <label>Email address<span>*</span></label>
+        <input type="email" placeholder="Marry Doe" />
+      </div>
+
+      <div className="form-group">
+        <label>Password<span>*</span></label>
+        <input type="password" placeholder="Marry Doe" />
+      </div>
+
+      <div className="form-group">
+        <label>Company name</label>
+        <input type="text" placeholder="Marry Doe" />
+      </div>
+
+      <div className="agency-section">
+        <p>Are you an Agency?<span>*</span></p>
+
+        <div className="radio-group">
+          <label>
+            <input type="radio" name="agency" defaultChecked />
+            Yes
+          </label>
+
+          <label>
+            <input type="radio" name="agency" />
+            No
+          </label>
+        </div>
+      </div>
+
+      <button
+        className="submit-btn"
+        onClick={() => navigate("/AccountSettings")}
       >
         Create Account
       </button>
@@ -37,5 +62,6 @@ function CreateAccount(){
     </div>
   );
 }
+
 
 export default CreateAccount;
