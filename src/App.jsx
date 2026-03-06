d29b680ec5e33c1123d03b49d5234eb8508e7ae9
sai krishna <saikrishnamamidi2003@gmail.com>
2026-03-06 11:35:25 +0530

    Install React Router and configure page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import{BrowserRouter, Routes, Route} from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import CreateAccount from "./pages/CreateAccount";
+import AccountSeting from "./pages/AccountSettings"
+
 function App(){
   return(
-    <>
-    <h1>Popx assignment</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" element = {<Welcome/>} />
+      <Route path = "/createAccount" element = {<CreateAccount/>} />
+      <Route path = "/AccountSetting" element = {<AccountSeting/>} />
+
+    </Routes>
+    </BrowserRouter>
   );
 }
 
