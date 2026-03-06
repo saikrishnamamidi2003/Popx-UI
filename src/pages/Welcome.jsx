04343612ab536943e99852941f4a99033558bc08
sai krishna <saikrishnamamidi2003@gmail.com>
2026-03-06 15:51:51 +0530

    Build Welcome page UI and navigation buttons

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,6 +1,28 @@
-function welcome(){
+import { useNavigate } from "react-router-dom";
+
+function Welcome(){
+    const navigate = useNavigate();
     return(
-        <h1>welcome page</h1>
+        <div className="welcome-container">
+            <div className="welcome-content">
+                <h1>Welcome to PopX</h1>
+                <p>Lorem ipusm dolor sit amet, consectetur asipiscing elit,
+                </p>
+
+                <button
+                 className="create-btn" 
+                 onClick={()=> navigate("/createAccount")}
+                 >
+                  Create Account
+                </button>
+
+                <button className="login-btn" onClick={()=> navigate("/accountSetting")}>
+                    Already Registered? Login
+                </button>
+
+            </div>
+
+        </div>
     );
 }
-export default welcome;
+export default Welcome;
